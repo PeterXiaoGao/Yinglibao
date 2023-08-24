efba411deff2c21c7f782a1411737d04b719ae25
PeterXiaoGao <G805370101@163.com>
2023-08-24 09:21:20 +0800

    前端vue
    理财  前端vue实现

---
+++ Yinglibao/front/licai/vue.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // 保存时是否进行语法检查。 true表示检查， false表示不检查。 默认值是true。
+  lintOnSave: false,
+
+})
